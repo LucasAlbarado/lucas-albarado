0e213bcda94c518cabbeb5a4dc27d767f8d99bdb
LucasAlbarado <lucas.miguel.albarado24@gmail.com>
2024-01-31 17:57:16 -0300

    Se agrego nav-bar

---
+++ src/components/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>View Home</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/layouts/navBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavBarExample = () => {
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            React-Bootstrap
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/About">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBarExample;
