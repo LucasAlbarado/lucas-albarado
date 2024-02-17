33736d164773d6276277d35088bd1ae2fd010584
Lucas Albarado <lucas.miguel.albarado@gmail.com>
2024-02-17 00:56:52 -0300

    login finish

--- src/components/loginRegister.jsx
+++ src/components/loginRegister.jsx
@@ -4,28 +4,91 @@ import "../components/loginRegister.css";
 import logo1 from "../images/logo1.png";
 import { FaGoogle, FaLinkedinIn, FaGithub } from "react-icons/fa";
 import Swal from "sweetalert2";
+import axios from "axios";
+
 
 function LoginRegister() {
   const [showRegisterForm, setShowRegisterForm] = useState(true);
+  //register
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+  //login
+  const [loginFormData, setLoginFormData] = useState({
+    name: "",
+    password: ""
+  });
 
   const handleSignInClick = () => {
     setShowRegisterForm(false);
   };
 
+  
   const handleSignUpClick = () => {
     setShowRegisterForm(true);
   };
-
-  const handleSuccessfulRegistration = (username) => {
-    setUsername(username);
-    setIsAuthenticated(true);
-    // Mostrar el mensaje con SweetAlert
-    Swal.fire({
-      title: "The Internet?",
-      text: "That thing is still around?",
-      icon: "question"
+  
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
     });
   };
+
+  const handleLoginChange = (e) => {
+    setLoginFormData({
+      ...loginFormData,
+      [e.target.name]: e.target.value
+    });
+  };
+  //login
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get("https://nodejs-mysql-apirest-8516156f22db.herokuapp.com/api/login", { params: loginFormData });
+      console.log(response.data);
+      
+      Swal.fire({
+        title: "Inicio de sesión exitoso",
+        text: `¡Bienvenido de nuevo, ${response.data.name}!!`,
+        icon: "success"
+      });
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        title: "Error",
+        text: "Ha ocurrido un error durante el inicio de sesión. Por favor, verifica tus credenciales e inténtalo de nuevo.",
+        icon: "error"
+      });
+    }
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://nodejs-mysql-apirest-8516156f22db.herokuapp.com/api/users", formData);
+      console.log(response.data);
+      // Aquí puedes agregar cualquier lógica adicional para manejar la respuesta, como mostrar una alerta de éxito
+      Swal.fire({
+        title: "Registro exitoso",
+        text: "¡Tu cuenta ha sido creada correctamente!",
+        icon: "success"
+      });
+    } catch (error) {
+      console.error(error);
+      // Aquí puedes manejar cualquier error que ocurra durante el registro
+      Swal.fire({
+        title: "Error",
+        text: "Ha ocurrido un error durante el registro. Por favor, inténtalo de nuevo más tarde.",
+        icon: "error"
+      });
+    }
+  };
+  //termina
+
 
   return (
     <div className="main">
@@ -84,16 +147,18 @@ function LoginRegister() {
                 </Link>
               </div>
               <p>USAR EMAIL PARA REGISTRARTE</p>
-              <form className="form" id="form-register">
+              <form onSubmit={handleSubmit} className="form" id="form-register">
                 {/* Contenido del formulario de registro */}
                 <label>
                   <i className="bx bx-user-circle"></i>
                   <input
                     type="text"
-                    name="user"
+                    name="name"
                     placeholder="Nombre de Usuario"
                     maxLength="30"
                     id="username-r"
+                    value={formData.name}
+                    onChange={handleChange}
                     required
                   />
                 </label>
@@ -105,6 +170,8 @@ function LoginRegister() {
                     placeholder="Correo Electrónico"
                     maxLength="256"
                     autoComplete="email"
+                    value={formData.email}
+                    onChange={handleChange}
                     id="email-r"
                     required
                   />
@@ -113,12 +180,14 @@ function LoginRegister() {
                   <i className="bx bx-lock"></i>
                   <input
                     type="password"
-                    name="contraseña"
+                    name="password"
                     placeholder="Contraseña"
                     minLength="8"
                     maxLength="16"
                     autoComplete="current-password"
                     id="password-r"
+                    value={formData.password}
+                    onChange={handleChange}
                     required
                   />
                 </label>
@@ -139,16 +208,18 @@ function LoginRegister() {
                   <FaLinkedinIn className="bx bxl-linkedin bx-flashing" />
                 </Link>
               </div>
-              <p>Inicia sesión con email</p>
-              <form className="form" id="form-login">
+              <p>Inicia sesión con el nombre se usuario</p>
+              <form onSubmit={handleLoginSubmit} className="form" id="form-login">
                 <label>
                   <i className="bx bx-user-circle"></i>
                   <input
                     type="text"
-                    name="user"
+                    name="name"
                     placeholder="Nombre de Usuario"
                     maxLength="30"
                     id="username-l"
+                    value={loginFormData.name}
+                    onChange={handleLoginChange}
                     required
                   />
                 </label>
@@ -156,12 +227,13 @@ function LoginRegister() {
                   <i className="bx bx-lock"></i>
                   <input
                     type="password"
-                    name="contraseña"
+                    name="password"
                     placeholder="Contraseña"
                     minLength="8"
                     maxLength="16"
-                    autoComplete="email"
                     id="password-l"
+                    value={loginFormData.password}
+                    onChange={handleLoginChange}
                     required
                   />
                 </label>
