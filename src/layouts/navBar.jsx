68a78bec6ed6ca3ba48ff0ad6adc8ebeb05fc68c
Lucas Albarado <lucas.miguel.albarado@gmail.com>
2024-02-20 14:10:02 -0300

    timer-navbar

--- src/layouts/navBar.jsx
+++ src/layouts/navBar.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { Outlet, Link  } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown  } from "react-bootstrap";
 import "./navBar.css";
@@ -6,10 +6,40 @@ import logo1 from "../images/logo1.png";
 
 const NavBarExample = ({ isAuthenticated, userName  }) => {
 
+  const [seconds, setSeconds] = useState( localStorage.getItem("seconds") ? parseInt(localStorage.getItem("seconds")) : 0);
+ 
+  const [minutes, setMinutes] = useState(
+    localStorage.getItem("minutes") ? parseInt(localStorage.getItem("minutes")) : 0
+  );
+  
+
+
+  let timer;
+
+  useEffect(() => {
+    timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+      if (seconds === 59) {
+        setMinutes((prevMinutes) => prevMinutes + 1);
+        setSeconds(0);
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [seconds]);
+  useEffect(() => {
+    // Almacenar segundos y minutos en localStorage
+    localStorage.setItem("seconds", seconds);
+    localStorage.setItem("minutes", minutes);
+  }, [seconds, minutes]);
+
+
   const handleLogout = () => {
     // Limpiar la sesión en el cliente
     localStorage.removeItem("isAuthenticated");
     localStorage.removeItem("userName");
+    setSeconds(0);
+  setMinutes(0);
     window.location.href = "/"; // Cambia la URL según sea necesario
   };
   const setUserName = () => {};
@@ -41,11 +71,14 @@ const NavBarExample = ({ isAuthenticated, userName  }) => {
               {/* Renderizado condicional para mostrar diferentes enlaces según el estado de autenticación */}
               {isAuthenticated ? (
             <NavDropdown title="Mi Cuenta" id="basic-nav-dropdown">
-            <NavDropdown.Item>Hola, {userName}</NavDropdown.Item>
+            <NavDropdown.Item className="pointer-nav-item">Hola, {userName}</NavDropdown.Item>
+            <NavDropdown.Item className="pointer-nav-item">
+            Tiempo en sesion {minutes}:{seconds}
+            </NavDropdown.Item>
+            <NavDropdown.Divider className="m-1"/>
             <NavDropdown.Item href="#action/3.2">
             Editar perfil
             </NavDropdown.Item>
-            <NavDropdown.Divider />
             <NavDropdown.Item onClick={handleLogout} as="button" href="#action/3.4">
               Cerrar sesion
             </NavDropdown.Item>
